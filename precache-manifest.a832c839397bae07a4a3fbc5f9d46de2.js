399e6e0739587c38d7f37b42ee6da4d922e9130f
William_MAC <william_martinsson@hotmail.com>
2018-11-30 12:32:39 +0100

    Updates

---
+++ precache-manifest.a832c839397bae07a4a3fbc5f9d46de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio-Playground/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e02d1105cacfcb4f72f",
+    "url": "/Portfolio-Playground/static/js/runtime~main.0e02d110.js"
+  },
+  {
+    "revision": "c77751a9381407db9b1a",
+    "url": "/Portfolio-Playground/static/js/main.c77751a9.chunk.js"
+  },
+  {
+    "revision": "f2a3c2e548b67b25c60c",
+    "url": "/Portfolio-Playground/static/js/1.f2a3c2e5.chunk.js"
+  },
+  {
+    "revision": "c77751a9381407db9b1a",
+    "url": "/Portfolio-Playground/static/css/main.e078145c.chunk.css"
+  },
+  {
+    "revision": "00178eb5c156da0d6a811448a91e7fa2",
+    "url": "/Portfolio-Playground/index.html"
+  }
+];
